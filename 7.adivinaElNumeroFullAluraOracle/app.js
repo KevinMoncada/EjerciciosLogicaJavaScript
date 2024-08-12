a33582665ce5b03d9319d0435f2e30b39e5e8e61
KevinMoncada <kevinma098@gmail.com>
2024-08-12 10:02:18 -0500

    fin repaso

--- 7.adivinaElNumeroFullAluraOracle/app.js
+++ 7.adivinaElNumeroFullAluraOracle/app.js
@@ -1,70 +1,60 @@
-let dynamicText = document.getElementById('textoDinamico');
-let inputNumber = document.getElementById(`numeroIngresado`);
-let tryButton = document.getElementById(`intentar`);
-let restartButton = document.getElementById(`reiniciar`);
-let maxAttempts = 100;
-let secretNumber;
-let attemptsCounter = 0;
-let drawnNumbers = [];
+let numeroSecreto;
+let numeroIngresado;
+let contadorDeIntentos = 0;
+let campoNumeroIngresado = document.querySelector('#numeroIngresado');
+let textoDinamico = document.querySelector('#textoDinamico');
+let botonReinicio = document.querySelector('#reiniciar');
+let botonInicio = document.querySelector('#intentar');
+let formulario = document.querySelector('form');
 
 
-function generateSecretNumber() {
-    if (drawnNumbers.length == maxAttempts) {
-        dynamicText.innerHTML = `Ya se sortearon Todos los números posibles!! :(`;
-        tryButton.setAttribute(`disabled`, true);
-        inputNumber.setAttribute('disabled', true);
-    } else {
-        secretNumber = Math.floor(Math.random() * maxAttempts) + 1;
-        if (drawnNumbers.includes(secretNumber)) {
-            return generateSecretNumber()
-        } else {
-            drawnNumbers.push(secretNumber);
-            return secretNumber;
-        }
+
+condicionesIniciales();
+
+//Eventos
+botonInicio.addEventListener('click', realizarComparacion);
+botonReinicio.addEventListener('click', condicionesIniciales);
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    realizarComparacion();
+});
+
+
+function generarNumeroSecreto() {
+    return parseInt((Math.random() * 100) + 1);
+}
+
+
+function condicionesIniciales() {
+    botonReinicio.setAttribute('disabled', true);
+    botonInicio.removeAttribute('disabled');
+    textoDinamico.textContent = 'Ingresa un número entero entre 1 y 100';
+    numeroSecreto = generarNumeroSecreto();
+    campoNumeroIngresado.value = "";
+    contadorDeIntentos = 0;
+}
+
+
+function realizarComparacion() {
+    contadorDeIntentos += 1;
+    numeroIngresado = parseInt(campoNumeroIngresado.value);
+    console.log(numeroIngresado);
+    console.log({ contadorDeIntentos });
+    if ((numeroIngresado < 1) || (numeroIngresado > 100)) {
+        textoDinamico.textContent = 'Error!! Ingrese un número entero valido dentro del rango establecido (de 1 a 100)';
+        campoNumeroIngresado.value = "";
+    } else if (numeroIngresado > numeroSecreto) {
+        textoDinamico.textContent = 'El numero secreto es menor';
+        campoNumeroIngresado.value = "";
+    } else if (numeroIngresado < numeroSecreto) {
+        textoDinamico.textContent = 'El numero secreto es mayor';
+        campoNumeroIngresado.value = "";
+    } else if (numeroIngresado === numeroSecreto) {
+        textoDinamico.textContent = `Felicidades!! Adivinaste el numero secreto(${numeroIngresado}) en ${contadorDeIntentos} intentos!!`;
+        botonReinicio.removeAttribute('disabled');
+        botonInicio.setAttribute('disabled', true);
+        campoNumeroIngresado.value = "";
     }
 }
 
-function startConditions() {
-    dynamicText.innerHTML = `Ingresa un Número entero entre 1 y 100`;
-    inputNumber.value = '';
-    tryButton.removeAttribute('disabled');
-    restartButton.setAttribute('disabled', true)
-    inputNumber.removeAttribute('disabled')
-    attemptsCounter = 0;
-    generateSecretNumber();
-    console.log(secretNumber);
-    console.log(drawnNumbers);
-}
-startConditions();
 
-
-//Start Event
-tryButton.addEventListener('click', process);
-function process() {
-    let evaluatedNumber = parseInt(inputNumber.value);
-    attemptsCounter += 1;
-    switch (true) {
-        case (evaluatedNumber < 0 || evaluatedNumber > 100):
-            dynamicText.innerHTML = `El número ingresado no es valido, el rango establecido es entre 1 y 100 (Intentos: ${attemptsCounter})`;
-            break;
-        case (evaluatedNumber === secretNumber):
-            dynamicText.innerHTML = `Felicidades!! Adivinaste el Número Secreto ${evaluatedNumber} (Intentos: ${attemptsCounter})`;
-            tryButton.setAttribute(`disabled`, true);
-            restartButton.removeAttribute(`disabled`);
-            inputNumber.setAttribute('disabled', true);
-            break;
-        case (evaluatedNumber > secretNumber):
-            dynamicText.innerHTML = `Error!! El número Secreto es Menor (Intentos: ${attemptsCounter})`;
-            break;
-        case (evaluatedNumber < secretNumber):
-            dynamicText.innerHTML = `Error!! El número Secreto es Mayor (Intentos: ${attemptsCounter})`;
-            break;
-    }
-    inputNumber.value = ''
-}
-
-//Re-start Event
-restartButton.addEventListener('click', restartGame)
-function restartGame() {
-    startConditions()
-}
